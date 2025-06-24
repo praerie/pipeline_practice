6c88b044490a50ef390eb225e333ba60954c026b
praerie <elisabethadelie@gmail.com>
2025-06-24 17:50:41 -0400

    added from_metadata method and basic metadata parsing

--- shot_data_demo.py
+++ shot_data_demo.py
@@ -19,10 +19,32 @@ class ShotData:
             return cls(*match.groups())
         return None
 
+    @classmethod
+    def from_metadata(cls, metadata):
+        if all(key in metadata for key in ["sequence", "shot", "camera", "version"]):
+            return cls(
+                metadata["sequence"],
+                metadata["shot"],
+                metadata["camera"],
+                metadata["version"]
+            )
+        return None
+
     def __repr__(self):
         return f"<ShotData {self.shot_id}>"
 
-# Example
+# shot data demo
 if __name__ == "__main__":
-    shot = ShotData.from_id("sq200_s100_cam001_v002")
-    print(shot)
+    print("From ID:")
+    shot1 = ShotData.from_id("sq200_s100_cam001_v002")
+    print(shot1)
+
+    print("\nFrom Metadata:")
+    metadata = {
+        "sequence": "sq200",
+        "shot": "s100",
+        "camera": "cam001",
+        "version": "v002"
+    }
+    shot2 = ShotData.from_metadata(metadata)
+    print(shot2)
